acfd22ae5522d3c1b07d3afcf3be945115e00875
yarko <yarko@DESKTOP-R40JDH6>
2025-08-11 13:35:50 +0300

    #Практика типів данних

--- Program.cs
+++ Program.cs
@@ -8,26 +8,13 @@ namespace learning
     {
         static void Main()
         {
-            sbyte number = 127; // ДУЖЕ сильно економить память діапазон від -128 до 127
-            //byte number = 255 ; //Зберігає число від 0 до 255 силько еконопить пам'ять
-            //short number = 23000; //Зберігає число від -32 768 до -32 767 еконопить пам'ять
-            //int number = 2147483647; // не економить память. Діапазон від -2 147 483 648 до 2 147 483 647
 
+            int num_1 = 0, num_2 = 0;
 
-            string word = " hello brother "; // Ну... просто слова
-
-            float num = 4.5f; //число і числа після коми
-
-            char symbol = '3'; //Може виводити лише 1 символ
-
-            bool isHappy = false; // Присвоєння True або False
-
-            Console.WriteLine(isHappy + word + number +".");
-
-
-
-
-            //Console.WriteLine("hello world");
+            num_1 = Convert.ToInt32(Console.ReadLine());
+            num_2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("First: "+ num_1 +". Second: " + num_2);
+            Console.ReadKey();
         }
     }
 
