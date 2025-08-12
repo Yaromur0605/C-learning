8e7578715d72f48da348412ddf662581d659a972
yarko <yarko@DESKTOP-R40JDH6>
2025-08-12 16:34:50 +0300

    if else(база)

--- Program.cs
+++ Program.cs
@@ -5,26 +5,31 @@ namespace learning
     {
         static void Main()
         {
-            //Console.WriteLine(Math.Abs(-20)); // Знайти модуль числа
-            //Console.WriteLine(Math.Ceiling(4.11f));  // округлити до більшого
-            //Console.WriteLine(Math.Floor(4.11f)); // округлити до меншого
-            //Console.WriteLine(Math.Round(4.11f)); // округлити
-            //Console.WriteLine(Math.Cos(4.11f)); // виводження косинуса
-            //Console.WriteLine(Math.Sin(4.11f)); // Виводження синуса
-            //Console.WriteLine(Math.Min(5, 0));   // знаходження мінімального числа між іншими
-            //Console.WriteLine(Math.Max(88, 0)); // знаходження максимального числа між іншими
-            //Console.WriteLine(Math.Pow(5, 2));  // Піднесення до степеня
-            //Console.WriteLine(Math.Sqrt(4.11f)); // Знайти квадратний корінь
 
+            int a = 5;
+            bool isHasCar = false;
 
-            Console.WriteLine("Веди радіус круга: ");
-            Double radius = Convert.ToDouble(Console.ReadLine());
-            Double area = Math.PI * Math.Pow(radius, 2);
-            Double Okr = Math.Round(area, 2);
-            Console.WriteLine("Площадь круга з радіусом {0} рівна {1} " , radius , Okr);
+            if (a > 7)
+            {
+                Console.WriteLine("a > 7");
+            }
+            else if (a < 5)
+            {
+                Console.WriteLine("a<5");
+
+            }
+            else if ((a == 5 || isHasCar) && a > 2)
+            {                                                       /* || Значить та. Скажемо якщо а = 5 ,але IsHasCar буде true 
+                                                                    код виконаєтья ,бо одна з умов виконана, але && значить що 2 умови мають  бути виконані */
+                                            
+                Console.WriteLine("a = 5");
+                Console.WriteLine("User has not car!");
+            }
+            else
+            {
+                Console.WriteLine("a != 5");
+            }
+
         }
-
-
-
     }
 }
