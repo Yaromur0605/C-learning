2824caf8ef2c885928711c176ec8a85971e57208
yarko <yarko@DESKTOP-R40JDH6>
2025-08-12 15:50:50 +0300

    Математика №2  (Math) + практика

--- Program.cs
+++ Program.cs
@@ -5,25 +5,23 @@ namespace learning
     {
         static void Main()
         {
+            //Console.WriteLine(Math.Abs(-20)); // Знайти модуль числа
+            //Console.WriteLine(Math.Ceiling(4.11f));  // округлити до більшого
+            //Console.WriteLine(Math.Floor(4.11f)); // округлити до меншого
+            //Console.WriteLine(Math.Round(4.11f)); // округлити
+            //Console.WriteLine(Math.Cos(4.11f)); // виводження косинуса
+            //Console.WriteLine(Math.Sin(4.11f)); // Виводження синуса
+            //Console.WriteLine(Math.Min(5, 0));   // знаходження мінімального числа між іншими
+            //Console.WriteLine(Math.Max(88, 0)); // знаходження максимального числа між іншими
+            //Console.WriteLine(Math.Pow(5, 2));  // Піднесення до степеня
+            //Console.WriteLine(Math.Sqrt(4.11f)); // Знайти квадратний корінь
 
-           
-            Console.Write("Ведіть число з комою: ");
-            //user_input = Convert.ToDouble(Console.ReadLine());
-           float user_input = float.Parse(Console.ReadLine());
 
-            float result;
-            result = user_input + 10f; 
-
-            result *= 5f; // добавляю 5
-            result ++; // ++ значить добавити 1, щоб відняти --
-            //result = user_input - 10f;
-            //result = user_input * 10f;
-            //result = user_input / 10f;
-            //result = user_input % 10f;
-
-            //int a =5, b =6;
-            //int res = a - b;
-            Console.WriteLine("Перемінна" + result);
+            Console.WriteLine("Веди радіус круга: ");
+            Double radius = Convert.ToDouble(Console.ReadLine());
+            Double area = Math.PI * Math.Pow(radius, 2);
+            Double Okr = Math.Round(area, 2);
+            Console.WriteLine("Площадь круга з радіусом {0} рівна {1} " , radius , Okr);
         }
 
 
