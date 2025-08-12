505cef4f258ebe0e6a24c44843188b1035b1c795
yarko <yarko@DESKTOP-R40JDH6>
2025-08-12 15:13:26 +0300

    Математичні дії (основи)

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Numerics;
-
-
+﻿
 namespace learning
 {
     class Program
@@ -9,15 +6,27 @@ namespace learning
         static void Main()
         {
 
-            int num_1 = 0, num_2 = 0;
+           
+            Console.Write("Ведіть число з комою: ");
+            //user_input = Convert.ToDouble(Console.ReadLine());
+           float user_input = float.Parse(Console.ReadLine());
 
-            num_1 = Convert.ToInt32(Console.ReadLine());
-            num_2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("First: "+ num_1 +". Second: " + num_2);
-            Console.ReadKey();
+            float result;
+            result = user_input + 10f; 
+
+            result *= 5f; // добавляю 5
+            result ++; // ++ значить добавити 1, щоб відняти --
+            //result = user_input - 10f;
+            //result = user_input * 10f;
+            //result = user_input / 10f;
+            //result = user_input % 10f;
+
+            //int a =5, b =6;
+            //int res = a - b;
+            Console.WriteLine("Перемінна" + result);
         }
+
+
+
     }
-
-
-
 }
